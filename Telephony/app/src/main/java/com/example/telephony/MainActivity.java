fa1aa2c8658685f71b7a8aac6c98dde7f6dfc4cd
hoang <hoangbrato@gmail.com>
2023-06-26 10:31:46 +0700

    hoang update

--- Telephony/app/src/main/java/com/example/telephony/MainActivity.java
+++ Telephony/app/src/main/java/com/example/telephony/MainActivity.java
@@ -55,14 +55,97 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+//    private void getPhoneNumber() {
+//        TelephonyManager telephonyManager = (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
+//
+//
+//        if (telephonyManager != null) {
+//            @SuppressLint("MissingPermission") String phoneNumber = telephonyManager.getLine1Number();
+//
+//            textViewPhoneNumber.setText("Phone Number: " + phoneNumber + "\nIMEI: " );
+//
+//        }
+//    }
+
     private void getPhoneNumber() {
         TelephonyManager telephonyManager = (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
         if (telephonyManager != null) {
             @SuppressLint("MissingPermission") String phoneNumber = telephonyManager.getLine1Number();
-            textViewPhoneNumber.setText(phoneNumber);
+
+
+//            String imei = telephonyManager.getImei();
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+            String networkTypeString = getNetworkTypeString(telephonyManager.getNetworkType());
+            String simStateString = getSimStateString(telephonyManager.getSimState());
+            String networkOperatorName = telephonyManager.getNetworkOperatorName();
+            String networkOperator = telephonyManager.getNetworkOperator();
+            String simCountryIso = telephonyManager.getSimCountryIso();
+
+            StringBuilder infoBuilder = new StringBuilder();
+            infoBuilder.append("Phone Number: ").append(phoneNumber).append("\n");
+//            infoBuilder.append("IMEI: ").append(imei).append("\n");
+            infoBuilder.append("Network Operator Name: ").append(networkOperatorName).append("\n");
+            infoBuilder.append("Network Operator: ").append(networkOperator).append("\n");
+            infoBuilder.append("SIM Country ISO: ").append(simCountryIso).append("\n");
+            infoBuilder.append("SIM State: ").append(simStateString).append("\n");
+            infoBuilder.append("Network Type: ").append(networkTypeString).append("\n");
+
+            textViewPhoneNumber.setText(infoBuilder.toString());
         }
     }
 
+
+    private String getNetworkTypeString(int networkType) {
+        switch (networkType) {
+            case TelephonyManager.NETWORK_TYPE_UNKNOWN:
+                return "Unknown";
+            case TelephonyManager.NETWORK_TYPE_GPRS:
+                return "GPRS";
+            case TelephonyManager.NETWORK_TYPE_EDGE:
+                return "EDGE";
+            case TelephonyManager.NETWORK_TYPE_UMTS:
+                return "UMTS";
+            case TelephonyManager.NETWORK_TYPE_HSDPA:
+                return "HSDPA";
+            case TelephonyManager.NETWORK_TYPE_HSUPA:
+                return "HSUPA";
+            case TelephonyManager.NETWORK_TYPE_HSPA:
+                return "HSPA";
+            case TelephonyManager.NETWORK_TYPE_LTE:
+                return "LTE";
+            case TelephonyManager.NETWORK_TYPE_NR:
+                return "5G NR";
+            default:
+                return "Unknown";
+        }
+    }
+    private String getSimStateString(int simState) {
+        switch (simState) {
+            case TelephonyManager.SIM_STATE_UNKNOWN:
+                return "Unknown";
+            case TelephonyManager.SIM_STATE_ABSENT:
+                return "Absent";
+            case TelephonyManager.SIM_STATE_PIN_REQUIRED:
+                return "PIN Required";
+            case TelephonyManager.SIM_STATE_PUK_REQUIRED:
+                return "PUK Required";
+            case TelephonyManager.SIM_STATE_NETWORK_LOCKED:
+                return "Network Locked";
+            case TelephonyManager.SIM_STATE_READY:
+                return "Ready";
+            default:
+                return "Unknown";
+        }
+    }
     private void makePhoneCall(String PhoneNumber){
 
         if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
